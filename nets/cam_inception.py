b5c0c1c2c05f0fa2c927c936d000d27435482c9d
koala0qoo <41674535+koala0qoo@users.noreply.github.com>
2018-08-23 06:05:08 +0800

    Update cam_inception.py

--- nets/cam_inception.py
+++ nets/cam_inception.py
@@ -26,16 +26,16 @@ def cam_inception(inputs, num_classes=number_of_classes, is_training=True, reuse
     inception_c_feature = net
     with tf.variable_scope('cam_classifier/A'):
         net = slim.conv2d(inception_c_feature, 1024, [3, 3],
-                          weights_initializer=tf.zeros_initializer,
+                          weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                           padding='SAME',
                           scope='conv1_3x3')
         net = slim.conv2d(net, 1024, [3, 3],
-                          weights_initializer=tf.zeros_initializer,
+                          weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                           padding='SAME',
                           scope='conv2_3x3')
         net = slim.conv2d(net, num_classes, [1, 1],
                           activation_fn=None,
-                          weights_initializer=tf.zeros_initializer,
+                          weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                           scope='conv3_1x1')
         end_points['features_A'] = net
         # GAP
