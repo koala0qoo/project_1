bb78b8b44218005bc78d0281a488abcc28b34c9e
koala0qoo <41674535+koala0qoo@users.noreply.github.com>
2018-08-22 20:33:03 +0800

    Update preprocessing_factory.py

--- preprocessing/preprocessing_factory.py
+++ preprocessing/preprocessing_factory.py
@@ -69,6 +69,7 @@ def get_preprocessing(name, is_training=False):
       'vgg_16': vgg_preprocessing,
       'vgg_19': vgg_preprocessing,
       'densenet': inception_preprocessing,
+      'cam_inception': inception_preprocessing
   }
 
   if name not in preprocessing_fn_map:
